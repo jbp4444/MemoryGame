dc0228818e2f43f36b18c903c9341c9c58a4f98b
jbp1 <jbp1@duke.edu>
2021-01-26 08:48:37 -0500

    first check in of old code

---
+++ main/cardInfo.lua
@@ -0,0 +1,62 @@
+
+local M = {}
+
+
+M.cardBacks = {
+	"cardBack_blue2",
+	"cardBack_blue3",
+	"cardBack_blue4",
+	"cardBack_blue5",
+	"cardBack_red2",
+	"cardBack_red3",
+	"cardBack_red4",
+	"cardBack_red5",
+	"cardBack_green2",
+	"cardBack_green3",
+	"cardBack_green4",
+	"cardBack_green5",
+}
+M.cardFronts = {
+	"cardBack_blue1",
+	"cardBack_green1",
+	"cardBack_red1",
+}
+M.cardFaces = {
+	"bear",
+	"buffalo",
+	"chick",
+	"chicken",
+	"cow",
+	"crocodile",
+	"dog",
+	"duck",
+	"elephant",
+	"frog",
+	"giraffe",
+	"goat",
+	"gorilla",
+	"hippo",
+	"horse",
+	"monkey",
+	"moose",
+	"narwhal",
+	"owl",
+	"panda",
+	"parrot",
+	"penguin",
+	"pig",
+	"rabbit",
+	"rhino",
+	"sloth",
+	"snake",
+	"walrus",
+	"whale",
+	"zebra",
+	"shipBeige_manned",
+	"shipBlue_manned",
+	"shipGreen_manned",
+	"shipPink_manned",
+	"shipYellow_manned",
+}
+
+return M
---
+++ main/cardProto.go
@@ -0,0 +1,90 @@
+components {
+  id: "cardProto"
+  component: "/main/cardProto.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "cardSlide"
+  component: "/main/cardSlide.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "front"
+  type: "sprite"
+  data: "tile_set: \"/assets/cards.atlas\"\n"
+  "default_animation: \"cardBack_blue1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "back"
+  type: "sprite"
+  data: "tile_set: \"/assets/cards.atlas\"\n"
+  "default_animation: \"cardBack_blue4\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "face"
+  type: "sprite"
+  data: "tile_set: \"/assets/animals.atlas\"\n"
+  "default_animation: \"cow\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
